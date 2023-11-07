01d9d562634378b2d62d24b9ff2ab5a247f9d978
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-11-07 13:17:17 +0700

    feat: server socket connection

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import argparse
+import socket
 
 def load_args():
     arg = argparse.ArgumentParser()
@@ -8,9 +9,30 @@ def load_args():
     args = arg.parse_args()
     return args
 
+def listen(port):
+    # create a socket object and connect to the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', port))
+    s.listen(5)
+    print('Listening on port', port)
+    return s
+
 def main():
     args = load_args()
     print(args)
+
+    s = listen(args.port)
+    while True:
+        # establish connection with client
+        c, addr = s.accept()
+        print('Got connection from', addr)
+        # receive data from client
+        data = c.recv(1024)
+        print('Received', repr(data))
+        # send data to client
+        c.send(data)
+        # close the connection
+        c.close()
 
 if __name__ == '__main__':
     main()
