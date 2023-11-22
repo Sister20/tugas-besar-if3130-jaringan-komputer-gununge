4474aef6cdd17765e1c358a6c5387dbea4da589c
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-11-22 12:42:20 +0700

    refactor(segmentflag): simplify operations

--- lib/SegmentFlag.py
+++ lib/SegmentFlag.py
@@ -7,25 +7,14 @@ DEFAULT_FLAG = 0x00
 class SegmentFlag:
     def __init__(self, flag: bytes) -> None:
         # Flag init
-        self.syn = False
-        self.ack = False
-        self.fin = False
-
-        # Init flag from flag variable
-        if flag & SYN_FLAG:
-            self.syn = True
-        if flag & ACK_FLAG:
-            self.ack = True
-        if flag & FIN_FLAG:
-            self.fin = True
+        self.syn = True if flag & SYN_FLAG else False
+        self.ack = True if flag & ACK_FLAG else False
+        self.fin = True if flag & FIN_FLAG else False
 
     def get_flag_bytes(self) -> bytes:
+        # Return flag bytes
         flag = DEFAULT_FLAG
-        if self.syn:
-            flag |= SYN_FLAG
-        if self.ack:
-            flag |= ACK_FLAG
-        if self.fin:
-            flag |= FIN_FLAG
-
+        flag |= (SYN_FLAG if self.syn else DEFAULT_FLAG)
+        flag |= (ACK_FLAG if self.ack else DEFAULT_FLAG)
+        flag |= (FIN_FLAG if self.fin else DEFAULT_FLAG)
         return flag
