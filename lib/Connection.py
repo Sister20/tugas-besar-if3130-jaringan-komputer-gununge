29d52d61801864791d2ef160f158e7031af600f2
MHEN2606 <matthew.mahendra@gmail.com>
2023-11-23 16:57:01 +0700

    feat: Menghindari tubes menjadi 0 (Change socket type)

--- lib/Connection.py
+++ lib/Connection.py
@@ -7,7 +7,7 @@ class Connection:
     def __init__(self, ip: str = None, port: int = None):
         self.ip = ip
         self.port = port
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__handler= callable(message=MessageInfo())
         print(self.__handler)
         
