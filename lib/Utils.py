a9bc3d4c6ae6025914c7d37938d49b127b48f2c9
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-11-28 23:51:07 +0700

    feat: loading bar

--- lib/Utils.py
+++ lib/Utils.py
@@ -5,18 +5,30 @@ import hashlib
 def breakdown_file(data: bytes) -> list:
     list_data = []
     hamming = Hamming()
-    halfPayload = PAYLOAD_SIZE // 2
-    
-    while (len(data) > 0):
-        if (len(data)) > halfPayload:
-            encodedData = hamming.breakdownDataToBytes(data[:halfPayload])
-            
-            list_data.append(encodedData)
-            data = data[halfPayload:]
+    half_payload = PAYLOAD_SIZE // 2
+    total_chunks = len(data) // half_payload
+    processed_chunks = 0
+
+    def update_progress_bar():
+        nonlocal processed_chunks
+        progress = processed_chunks / total_chunks
+        bar_length = 40
+        bar = '=' * int(bar_length * progress) + '-' * (bar_length - int(bar_length * progress))
+        print(f"\rConverting to bytes: [{bar}] {int(progress * 100)}%", end='', flush=True)
+
+    while len(data) > 0:
+        if len(data) > half_payload:
+            encoded_data = hamming.breakdownDataToBytes(data[:half_payload])
+            list_data.append(encoded_data)
+            data = data[half_payload:]
         else:
-            encodedData = hamming.breakdownDataToBytes(data)
-            list_data.append(encodedData)
+            encoded_data = hamming.breakdownDataToBytes(data)
+            list_data.append(encoded_data)
             data = b''
+        processed_chunks += 1
+        update_progress_bar()
+
+    print()  # Print a new line after the progress bar is complete
     return list_data
 
 def merge_file(list_data: list) -> bytes:
